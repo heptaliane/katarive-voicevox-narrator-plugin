b342653236b02bbe2f19c3067bce3b9c7c57f4cd
heptaliane <heptaliane@gmail.com>
2026-05-02 18:29:13 +0900

    feat: Add VoiceVox narration functionality
    This commit introduces the core functionality for VoiceVox narration.
    
    - Implements `VoiceVoxHandler` interface and `HttpVoiceVoxHandler` for interacting with the VoiceVox API.
    - Provides methods to narrate text with customizable speaker names and styles.
    - Includes error handling for VoiceVox connection issues and unsupported speakers.
    - Updates `go.mod` and `go.sum` to include new dependencies required for OpenAPI client generation and related utilities.

--- go.mod
+++ go.mod
@@ -3,14 +3,17 @@ module github.com/heptaliane/katarive-voicevox-narrator-plugin
 go 1.26.1
 
 require (
+	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
 	github.com/fatih/color v1.13.0 // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/hashicorp/go-hclog v1.6.3 // indirect
 	github.com/hashicorp/go-plugin v1.7.0 // indirect
 	github.com/hashicorp/yamux v0.1.2 // indirect
 	github.com/heptaliane/katarive-go-sdk v0.0.13 // indirect
-	github.com/mattn/go-colorable v0.1.12 // indirect
-	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/oapi-codegen/runtime v1.4.0 // indirect
 	github.com/oklog/run v1.1.0 // indirect
 	golang.org/x/net v0.48.0 // indirect
 	golang.org/x/sys v0.39.0 // indirect
---
+++ voicevox.go
@@ -0,0 +1,165 @@
+package main
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/hashicorp/go-hclog"
+
+	"github.com/heptaliane/katarive-voicevox-narrator-plugin/gen/voicevox"
+)
+
+const DEFAULT_SPEAKER_NAME string = "ずんだもん"
+const DEFAULT_SPEAKER_STYLE string = "ノーマル"
+
+// ==============================
+// Interfaces for VoiceVox handlers
+// ==============================
+type VoiceVoxHandler interface {
+	Narrate(ctx context.Context, text string, opts ...VoiceVoxOption) ([]byte, error)
+}
+
+// -----------------
+// Helper components
+// -----------------
+
+type voiceVoxOption struct {
+	speakerName  string
+	speakerStyle string
+}
+
+type VoiceVoxOption func(opt *voiceVoxOption)
+
+func WithSpeakerName(name string) VoiceVoxOption {
+	return func(opt *voiceVoxOption) {
+		opt.speakerName = name
+	}
+}
+func WithSpeakerStyle(style string) VoiceVoxOption {
+	return func(opt *voiceVoxOption) {
+		opt.speakerStyle = style
+	}
+}
+
+func newVoiceVoxOption() *voiceVoxOption {
+	return &voiceVoxOption{
+		speakerName:  DEFAULT_SPEAKER_NAME,
+		speakerStyle: DEFAULT_SPEAKER_STYLE,
+	}
+}
+
+// ============================
+// VoiceVoxHandler Implementation
+// ============================
+
+// ----------------------
+// HttpVoiceVoxHandler
+// ----------------------
+type HttpVoiceVoxHandler struct {
+	speakers []*voiceVoiceSpeaker
+	client   voicevox.ClientWithResponsesInterface
+
+	logger hclog.Logger
+}
+
+func (h *HttpVoiceVoxHandler) Narrate(
+	ctx context.Context,
+	text string,
+	opts ...VoiceVoxOption,
+) ([]byte, error) {
+	options := newVoiceVoxOption()
+	for _, opt := range opts {
+		opt(options)
+	}
+
+	speaker, err := h.getVoiceVoxId(options)
+	if err != nil {
+		return nil, err
+	}
+
+	aqp := &voicevox.AudioQueryParams{
+		Text:    text,
+		Speaker: speaker,
+	}
+	aq, err := h.client.AudioQueryWithResponse(ctx, aqp)
+	if err != nil {
+		return nil, err
+	}
+	if aq.StatusCode() != http.StatusOK {
+		return nil, &VoiceVoxConnectionError{Body: aq.Body}
+	}
+
+	sp := &voicevox.SynthesisParams{
+		Speaker: speaker,
+	}
+	res, err := h.client.SynthesisWithResponse(ctx, sp, *aq.JSON200)
+	if err != nil {
+		return nil, err
+	}
+	if res.StatusCode() != http.StatusOK {
+		return nil, &VoiceVoxConnectionError{Body: res.Body}
+	}
+
+	return res.Body, nil
+}
+
+// Ensure VoiceVoxHandler implementation
+var _ VoiceVoxHandler = new(HttpVoiceVoxHandler)
+
+// -----------------
+// Helper components
+// -----------------
+type voiceVoiceSpeaker struct {
+	name  string
+	style string
+	id    int
+}
+
+func (h *HttpVoiceVoxHandler) getVoiceVoxId(opts *voiceVoxOption) (int, error) {
+	for _, speaker := range h.speakers {
+		if opts.speakerName == speaker.name && opts.speakerStyle == speaker.style {
+			return speaker.id, nil
+		}
+	}
+	return 0, &UnsupportedSpeakerError{
+		Name:  opts.speakerName,
+		Style: opts.speakerStyle,
+	}
+}
+
+func NewHttpVoiceVoxHandler(
+	ctx context.Context,
+	server string,
+	logger hclog.Logger,
+) (*HttpVoiceVoxHandler, error) {
+	client, err := voicevox.NewClientWithResponses(server)
+	if err != nil {
+		return nil, err
+	}
+
+	sp := &voicevox.SpeakersParams{}
+	res, err := client.SpeakersWithResponse(ctx, sp)
+	if err != nil {
+		return nil, err
+	}
+	if res.StatusCode() != http.StatusOK {
+		return nil, &VoiceVoxConnectionError{Body: res.Body}
+	}
+
+	var speakers []*voiceVoiceSpeaker
+	for _, speaker := range *res.JSON200 {
+		for _, style := range speaker.Styles {
+			speakers = append(speakers, &voiceVoiceSpeaker{
+				name:  speaker.Name,
+				style: style.Name,
+				id:    style.Id,
+			})
+		}
+	}
+
+	return &HttpVoiceVoxHandler{
+		client:   client,
+		speakers: speakers,
+		logger:   logger,
+	}, nil
+}
